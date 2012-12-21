73f8f2dc284365bf4b26af51dd5d943acee9353d
jcerise <jcerise06@gmail.com>
2012-12-20 23:40:28 -0700

    Added map generation algorithm. Pretty simple, just creates a bunch of rooms in random positions and sizes, then checks to see if they overlap another. If they do not, the room is placed, and connected to the previous room via horizontal and vertical tunnels. This can generate some decently interesting room layouts. Its starting to look like a game!

--- DungeonCrawler.py
+++ DungeonCrawler.py
@@ -13,9 +13,14 @@ MAP_HEIGHT = 45
 #Set the framerate limit. We do not use this, as our game is turn based, not real time
 LIMIT_FPS = 20
 
+#Set parameters for room generation
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 4
+MAX_ROOMS = 30
+
 #Set the colors for floor and wall tiles
-color_dark_wall = libtcod.Color(105, 105, 105)
-color_dark_ground = libtcod.Color(211, 211, 211)
+color_dark_wall = libtcod.Color(85, 85, 85)
+color_dark_ground = libtcod.Color(117, 104, 87)
 
 class Rect:
     #A rectangle on the map, used to represent a room
@@ -25,8 +30,23 @@ class Rect:
         self.x2 = x + w
         self.y2 = y + h
 
+    def center(self):
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        #Check if this rect intersects (overlaps) another rect
+        #if the top left corner is less than the bottom right corner of the other rect,
+        #and the bottom right corner is greater than the top right of the other rect,
+        #the rects intersect
+        return(self.x1 <= other.x2 and self.x2 >= other.x1 and
+               self.y1 <= other.y2 and self.y2 >= other.y1)
+
 def make_map():
     global map
+    global player_start_x
+    global player_start_y
 
     #Fill map with blocked tiles, this will allow us to 'carve' rooms for the player
     #to explore
@@ -34,11 +54,60 @@ def make_map():
         for y in range(MAP_HEIGHT) ]
             for x in range(MAP_WIDTH) ]
 
-    room1 = Rect(20, 15, 10, 15)
-    room2 = Rect(50, 15, 10, 20)
-    create_room(room1)
-    create_room(room2)
-    create_h_tunnel(25, 55, 23)
+    rooms = []
+    num_rooms = 0
+
+    for r in range(MAX_ROOMS):
+        #Generate a random width and height for each room
+        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+
+        #Generate a random map position, inside the bounds of the map
+        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+        y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h -1)
+
+        #Create a new room from the random numbers
+        new_room = Rect(x, y, w, h)
+
+        #Run through all the other rooms and see if they intersect with this one
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+
+        if not failed:
+            #If we've gotten here, the room is valid, and does not intersect any other rooms
+            #Carve the room into the maps tiles
+            create_room(new_room)
+
+            #Get the center coordinates for the new room
+            (new_x, new_y) = new_room.center()
+
+            if num_rooms == 0:
+                #This is the first room created, so start the player off in the center
+                player_start_x = new_x
+                player_start_y = new_y
+            else:
+                #This is not the first room, so connect it to the previous room via tunnels
+
+                #Get the center coordinates for the previous room
+                (prev_x, prev_y) = rooms[num_rooms - 1].center()
+
+                #Flip a coin to see if we move horizontally, or vertically first
+                if libtcod.random_get_int(0, 0, 1) == 1:
+                    #Tunnel horizontally first, then vertically
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_x)
+                else:
+                    #Tunnel vertically first, then horizontally
+                    create_v_tunnel(prev_y, new_y, prev_x)
+                    create_h_tunnel(prev_x, new_x, new_y)
+
+            #Finally, append the newly added room to the rooms list
+            rooms.append(new_room)
+            num_rooms += 1
+
 
 def create_room(room):
     global map
@@ -74,6 +143,14 @@ def handle_keys():
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         #ALT + Enter, toggle fullscreen
         libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+    elif key.vk == libtcod.KEY_SHIFT and key.lalt:
+        #Redraw the map, and move the player to the first room
+        #Debug and testing purposes only
+        map = []
+        make_map()
+        player.x = player_start_x
+        player.y = player_start_y
+
     elif key.vk == libtcod.KEY_ESCAPE:
         #Escape, exit game
         return True
@@ -126,9 +203,11 @@ make_map()
 
 #Create our objects, in this case just a player, then add them to the objects array
 player = Object(con, map, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', libtcod.white)
-player.x = 25
-player.y = 23
-objects = [player]
+player.x = player_start_x
+player.y = player_start_y
+
+objects = []
+objects.insert(0, player)
 
 while not libtcod.console_is_window_closed():
     #Render the map, and all objects
