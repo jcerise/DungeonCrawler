b996124e10a65fcbe4f590d61e27a7dbd13e7323
Jeremy Cerise <jcerise06@gmail.com>
2012-12-30 16:09:54 -0700

    Added system to add monsters via XML. Monsters are defined in XML files per level (which corresponds to levels of the dungeon). Each monster has a chance of appearing, and the higher the chance, the more the player will encounter. These monsters are then dynamically loaded, created, and added to the dungeon.

--- DungeonCrawler.py
+++ DungeonCrawler.py
@@ -1,4 +1,5 @@
 import libtcodpy as libtcod
+import xml.etree.ElementTree as ET
 import textwrap
 from gameObject import *
 from tile import Tile
@@ -142,6 +143,9 @@ def create_v_tunnel(y1, y2, x):
         map[x][y].block_sight = False
 
 def place_objects(room):
+    global monsters
+    global monster_appearance_chances
+
     num_monsters = libtcod.random_get_int(0, 0, MAX_ROOM_MONSTERS)
 
     for i in range(num_monsters):
@@ -149,19 +153,48 @@ def place_objects(room):
         y = libtcod.random_get_int(0, room.y1, room.y2)
 
         if not is_blocked(x, y):
-            #80 percent chance of an orc spawning
+            #Choose a monster to spawn from the list of applicable monsters
+            spawn = random_choice_index(monster_appearance_chances)
+
+            #Choose the monster based on the spawn chance
+            monster = monsters[spawn]
+
+            #Create a death function for the monster
             monster_death = getattr(Fighter, 'monster_death')
-            if libtcod.random_get_int(0, 0, 100) < 80:
-                #Create an orc
-                fighter_component = Fighter(hp = 10, defense = 0, power = 3, death_function = monster_death)
+
+            #Create a component for the monster based on the monster type
+            if monster[1] == 'fighter':
+                fighter_component = Fighter(hp = int(monster[3]), defense = int(monster[4]), power = int(monster[5]),
+                    death_function = monster_death)
+
+            #Create an AI component for the monster based on its AI type
+            if monster[2] == 'basic':
                 ai_component = BasicMonster()
-                monster = Object(x, y, 'o', 'Orc', libtcod.desaturated_green, True, fighter_component, ai_component)
-            else:
-                fighter_component = Fighter(hp = 16, defense = 1, power = 4, death_function=monster_death)
-                ai_component = BasicMonster()
-                monster = Object(x, y, 'T', 'Troll', libtcod.darker_green, True, fighter_component, ai_component)
+
+            #Finally, create the monster
+            monster = Object(x, y, char = monster[6], name = monster[0], color = libtcod.Color(int(monster[7]),
+                int(monster[8]), int(monster[9])), blocks = True, fighter = fighter_component,  ai = ai_component)
+
             #Add the monster to the objects array
             objects.append(monster)
+
+def random_choice_index(chances):
+    #Choose an option from a list of chances
+    #Roll a die, whcih will land somewhere between 1 and the total of the chances list
+    #Then, we simply need to figure out what range in the list that number falls under
+    dice = libtcod.random_get_int(0, 1, sum(chances))
+
+    running_sum = 0
+    choice = 0
+    #Go through all chances, keeping the total so far
+    for w in chances:
+        running_sum += w
+
+        #Check if the die roll is smaller or equal to the current total.
+        #If it is, we know the chance range the roll fell into
+        if dice <= running_sum:
+            return choice
+        choice += 1
 
 def is_blocked(x, y):
     #Test the map tile at the coordinates to see if it is blocked or not
@@ -363,6 +396,41 @@ libtcod.sys_set_fps(LIMIT_FPS)
 
 #Initialize the objects array
 objects = []
+
+#Figure out which monsters to include on this level, and load them all
+#TODO: For each level, the player can encounter monsters from the previous level, as well as the next level
+#There is a 50% decrease in encounter chances for previous level monsters
+#There is a 75% decrease in encounter chances for next level monsters
+#This will keep the chance of difficult monsters a possibility on any level
+monster_tree = ET.parse('monsters/level-0.xml')
+monster_root = monster_tree.getroot()
+
+monsters = []
+
+#Create a list with all the applicable monsters for this floor
+for monster in monster_root.findall('monster'):
+    m = []
+    m.append(monster.get('name'))
+    m.append(monster.find('type').text)
+    m.append(monster.find('ai-type').text)
+    m.append(monster.find('hit-points').text)
+    m.append(monster.find('defense').text)
+    m.append(monster.find('attack-power').text)
+    m.append(monster.find('character').text)
+    m.append(monster.find('color-r').text)
+    m.append(monster.find('color-g').text)
+    m.append(monster.find('color-b').text)
+    m.append(monster.find('encounter-chance').text)
+
+    #Add the newly created monster list to the list of monsters
+    monsters.append(m)
+
+#Next, build a list with just the monster appearance chances, so we can figure out later how often each monster will
+#show up. The index of the chance in this list directly corresponds to the index of the monster in the monsters list
+monster_appearance_chances = []
+
+for appearing_monster in monsters:
+    monster_appearance_chances.append(int(appearing_monster[10]))
 
 #Create the map
 make_map()
